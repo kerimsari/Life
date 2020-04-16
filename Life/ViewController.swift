1a1f1f7d4bca3632673ecbb8bee83eeb4b13ab00
CarlosAlberto <kerimsari@gmail.com>
2020-04-16 15:08:18 +0300

    Initial Commit

---
+++ Life/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Life
+//
+//  Created by Elif on 16.04.2020.
+//  Copyright © 2020 kerimsari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
